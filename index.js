68232062ba0614fc0c91c8b40a0678f820aab7bb
TheBlackyOn <43524748+TheBlackyOn@users.noreply.github.com>
2018-12-13 03:41:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+let prefix = config.prefix;
+
+client.on("ready", () => {
+   console.log("¡Estoy preparado!");
+});
+
+client.on("message", (message) => {
+  if(message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+
+  } else
+  if(message.content.startsWith(prefix + "hola")) { 
+     message.channel.send("Hola que tal?");
+
+  }
+
+});
+
+client.login(config.token);
